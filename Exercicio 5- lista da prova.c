1060ea13f7c060f087beabf978c48de977030468
HELOisef5 <heloisefurtado5@gmail.com>
2026-03-30 21:22:21 -0300

    Update Exercicio 5- lista da prova.c

--- Exercicio 5- lista da prova.c
+++ Exercicio 5- lista da prova.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 main(void){
 	int med1, med2, med3;
-	med1= 8+9+7/3;
-	med2= 4+5+6/3;
-	med3= med1+med2/2;
+	med1=(8+9+7)/3;
+	med2= (4+5+6)/3;
+	med3= (med1+med2)/2;
 	printf("A media das medias e %d", med3);
 }
